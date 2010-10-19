0959acda3c6f786693a61641375e3f656108861e
hooyes@gmail.com <hooyes@gmail.com@92b1ee02-ebb8-11de-91ba-91c70a792394>
2010-10-19 09:24:47 +0000

    hooyes wcf project start

---
+++ trunk/hooyes.Web/hooyes.Wcf/hooyes.WCF.Host/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+namespace hooyes.WCF.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostOpen();
+        }
+        static void HostOpen()
+        {
+            ServiceHost host = new ServiceHost(typeof(WCF.Service.Sv));
+            host.Open();
+            Console.WriteLine("service");
+            Console.ReadKey();
+            host.Close();
+        }
+    }
+}
---
+++ trunk/hooyes.Web/hooyes.Wcf/hooyes.WCF.Service/Sv.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hooyes.WCF.Service
+{
+    public class Sv:ISv
+    {
+        public string GetData(int value)
+        {
+            return string.Format("you input {0}", value);
+        }
+    }
+}
