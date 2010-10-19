8314b0c1f38504d7fa55570adf3558e4064f126f
hooyes <hooyes@gmail.com>
2010-10-19 09:24:47 +0000

    hooyes wcf project start

---
+++ hooyes.Web/hooyes.Wcf/hooyes.WCF.Service/ISv.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+namespace hooyes.WCF.Service
+{
+   [ServiceContract]
+   public interface ISv
+    {
+       [OperationContract]
+       string GetData(int value);
+    }
+}
